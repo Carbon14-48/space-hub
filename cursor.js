e46c6ac9afc08d2e9fc78c461321be5d169d00b0
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-08-14 13:41:19 +0100

    done

--- cursor.js
+++ cursor.js
@@ -2,12 +2,18 @@ const circleEl = document.querySelector(".circle");
 const currentPoint = { x: 0, y: 0 };
 const targetPoint = { x: 0, y: 0 };
 
-function lerp() {}
+function lerp() {
+  const dx = targetPoint.x - currentPoint.x;
+  const dy = targetPoint.y - currentPoint.y;
+  currentPoint.x += dx * 0.2;
+  currentPoint.y += dy * 0.2;
+  circleEl.style.setProperty("--x", currentPoint.x);
+  circleEl.style.setProperty("--y", currentPoint.y);
+  requestAnimationFrame(lerp);
+}
+lerp();
 document.addEventListener("pointermove", (e) => {
   const { clientX, clientY } = e;
   targetPoint.x = clientX;
   targetPoint.y = clientY;
-
-  circleEl.style.setProperty("--x", clientX);
-  circleEl.style.setProperty("--y", clientY);
 });
